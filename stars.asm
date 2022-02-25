80b7e37fb955434fc92a0155fedd6508df3e25f2
DylanMossCambridge <77005990+DylanMossCambridge@users.noreply.github.com>
2022-02-25 23:02:56 +0000

    Update stars.asm

--- stars.asm
+++ stars.asm
@@ -1,22 +1,22 @@
-section .data
+section .data                                ; store string constants
   question db "How many lines?", 10
   star db "*"
   newline db "", 10
 
-section .bss 
+section .bss                                 ; store buffer area for user input 
   lines resb 1
 
-section .text
+section .text                                ; program start point 
   global _start
 
 _start:
   call _printNewLine
   call _printQuestion
   call _getLines
-  mov r8, r12
-  mov r9, 0
+  mov r8, r12                                ; r8 stores number of lines 
+  mov r9, 0                                  ; r9 stores outer loop variable 
   cmp r8, r9
-  jz _exit
+  jz _exit                 
   call _outerLoop
 
 _exit:
@@ -27,10 +27,10 @@ _exit:
 
 _outerLoop:
   call _printNewLine
-  cmp r8, r9
-  jz _exit
+  cmp r8, r9                                 ; checks if r8 = r9 
+  jz _exit                                   ; jump to exit if the result is 0 (ie. they are equal) 
   add r9, 1
-  mov r10, 0
+  mov r10, 0                                 ; r10 stores inner loop variable 
   jmp _innerLoop
 
 _innerLoop: 
@@ -41,11 +41,11 @@ _innerLoop:
   jmp _innerLoop
 
 _printQuestion:
-  mov rax, 1
-  mov rdi, 1
-  mov rsi, question
-  mov rdx, 16
-  syscall
+  mov rax, 1                                  ; sys_write 
+  mov rdi, 1                                  ; stdout 
+  mov rsi, question                           ; word to write 
+  mov rdx, 16                                 ; number of bytes to write
+  syscall                                     ; call system to write word to stdout
   ret
 
 _printNewLine:
@@ -57,7 +57,7 @@ _printNewLine:
   ret
 
 _printStar:
-  mov rax, 1
+  mov rax, 1                                
   mov rdi, 1
   mov rsi, star
   mov rdx, 1
@@ -65,11 +65,11 @@ _printStar:
   ret
 
 _getLines: 
-  mov rax, 0 
-  mov rdi, 0
-  mov rsi, lines
-  mov rdx, 1
-  syscall
+  mov rax, 0                                 ; sys_read
+  mov rdi, 0                                 ; stdin 
+  mov rsi, lines                             ; buffer space to read word 
+  mov rdx, 1                                 ; number of bytes to read
+  syscall                                    ; call system to read word from stdin 
   mov r12, [rsi]
-  sub r12, 48
+  sub r12, 48                                ; convert ascii string to int 
   ret                  
